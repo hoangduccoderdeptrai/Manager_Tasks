c81019ca946e873602e09eacb2d673cbdf09d48e
ducdeptrai <duclehoangcoder@gmail.com>
2023-11-13 21:49:54 +0700

    update

--- 22520351.sql
+++ 22520351.sql
@@ -1,13 +1,11 @@
 -- 1. BÀI TẬP 1
 --III.12
--- 12.	Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, 
--- mỗi sản phẩm mua với số lượng từ 10 đến 20.
 
--- SELECT SOHD FROM CTHD 
+-- SELECT DISTINCT SOHD FROM CTHD 
 -- WHERE MASP IN('BB01','BB02') AND SL BETWEEN 10 AND 20
 
--- 13.	Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, 
--- mỗi sản phẩm mua với số lượng từ 10 đến 20.
+-- III.13
+
 -- SELECT SOHD FROM CTHD 
 -- WHERE MASP ='BB01' AND SL BETWEEN 10 AND 20
 -- INTERSECT
@@ -15,30 +13,29 @@
 -- WHERE MASP ='BB02' AND SL BETWEEN 10 AND 20
 
 -- 2.BÀI TẬP 2
---1.	Tăng hệ số lương thêm 0.2 cho những giáo viên là trưởng khoa.
+-- II.1
 
 -- UPDATE GIAOVIEN
 -- SET MUCLUONG =1.2*MUCLUONG
 -- WHERE GIAOVIEN.MAGV IN (SELECT DISTINCT TRGKHOA FROM KHOA  )
 
--- 2.	Cập nhật giá trị điểm trung bình tất cả các môn học (DIEMTB) 
--- của mỗi học viên (tất cả các môn học đều có hệ số 
--- 1 và nếu học viên thi một môn nhiều lần, chỉ lấy điểm của lần thi sau cùng).
+-- II.2
+-- WITH TEMP AS(
+--     SELECT KQ.MAHV,AVG(KQ.DIEM) AS DIEMTB FROM KETQUATHI AS KQ
+--     INNER JOIN (
+--         SELECT MAHV,MAMH,MAX(LANTHI) AS  MAX_LANTHI FROM KETQUATHI
+--         GROUP BY MAHV,MAMH
 
+--     ) AS KQ_MAX
+--     ON KQ.MAHV =KQ_MAX.MAHV AND KQ.LANTHI =KQ_MAX.MAX_LANTHI AND KQ.MAMH =KQ_MAX.MAMH
+--     GROUP BY KQ.MAHV
+-- )
+-- UPDATE HOCVIEN
+-- SET HOCVIEN.DIEMTB =(SELECT DIEMTB FROM TEMP
+--     WHERE HOCVIEN.MAHV=TEMP.MAHV
+-- ) 
 
-
--- SELECT MAHV,AVG(DIEM) AS DIEMTB FROM KETQUTHI AS KQ
--- INNER JOIN (
---     SELECT MAHV,MAMH,MAX(LANTHI) AS  MAX_LANTHI FROM KETQUATHI
---     GROUP BY MAHV,MAMH
-
--- ) AS KQ_MAX
--- ON KQ.MAHV =KQ_MAX.MAHV AND KQ.LANTHI =KQ_MAX.MAX_LANTHI AND KQ.MAMH =KQ_MAX.MAMH
--- GROUP BY KQ.MAHV
-
-
--- 3.	Cập nhật giá trị cho cột GHICHU là “Cam thi”
---  đối với trường hợp: học viên có một môn bất kỳ thi lần thứ 3 dưới 5 điểm.
+-- II.3	
 
 -- UPDATE HOCVIEN
 -- SET GHICHU='Cam thi'
@@ -47,7 +44,8 @@
 --     WHERE LANTHI=3 AND DIEM <5
 -- )
 
---4.
+--II.4
+
 -- UPDATE HOCVIEN
 -- SET XEPLOAI = CASE
 --     WHEN DIEMTB>= 9 THEN 'XS'
@@ -58,9 +56,7 @@
 --     END
 
 -- 3.BÀI TẬP 3
---6.
--- 6.	Tìm tên những môn học mà giáo viên có 
--- tên “Tran Tam Thanh” dạy trong học kỳ 1 năm 2006.
+-- III.6
 
 -- SELECT TENMH FROM MONHOC AS MH
 -- CROSS JOIN GIAOVIEN AS GV
@@ -68,8 +64,7 @@
 -- WHERE GV.HOTEN ='Tran Tam Thanh' AND GD.HOCKY=1 AND NAM=2006
 
 
--- 7.	Tìm những môn học (mã môn học, tên môn học)
---  mà giáo viên chủ nhiệm lớp “K11” dạy trong học kỳ 1 năm 2006.
+-- III.7
 -- SELECT MAMH,TENMH FROM MONHOC
 -- WHERE MAMH IN(
 --     SELECT MAMH FROM GIANGDAY
@@ -77,8 +72,8 @@
 --     WHERE LOP.MALOP ='K11' AND HOCKY=1 AND NAM=2006
 -- )
 
---8.	Tìm họ tên lớp trưởng của các lớp mà giáo
--- viên có tên “Nguyen To Lan” dạy môn “Co So Du Lieu”.
+--III.8
+-- c1
 
 -- SELECT HO + ' ' + TEN AS HOTEN FROM HOCVIEN
 -- WHERE MAHV IN (
@@ -93,19 +88,21 @@
 -- 	)
 -- )
 
+-- c2
 
 -- SELECT HO+' '+TEN FROM HOCVIEN AS HV
 -- WHERE HV.MAHV IN (
 --     SELECT LOP.TRGLOP FROM LOP
 --     CROSS JOIN GIAOVIEN AS GV
 --     INNER JOIN MONHOC ON LOP.MAGVCN =GV.MAGV AND GV.MAKHOA =MONHOC.MAKHOA
---     WHERE GV.HOTEN ='Nguyen To Lan' and GV.TENMH='Co So Du Lieu'
+--     WHERE GV.HOTEN ='Nguyen To Lan' and MONHOC.TENMH='Co So Du Lieu'
 
 
 -- )
 
---9.	In ra danh sách những môn học (mã môn học, tên môn học)
+--III.9 In ra danh sách những môn học (mã môn học, tên môn học)
 -- phải học liền trước môn “Co So Du Lieu”
+-- C1
 
 -- SELECT MAMH,TENMH FROM MONHOC AS MH
 -- INNER JOIN DIEUKIEN AS DK ON DK.MAMH_TRUOC =MH.MAMH
@@ -113,8 +110,9 @@
 --     SELECT MAMH FROM MONHOC
 --     WHERE TENMH ='Co So Du Lieu'
 -- )
+-- C2
 
--- SELECT MAMH,TENMH FROM MONHOC AS MH 
+-- SELECT MH.MAMH,TENMH FROM MONHOC AS MH 
 -- WHERE MH.MAMH IN(
 --     SELECT MAMH_TRUOC FROM DIEUKIEN AS DK
 --     WHERE DK.MAMH IN(
@@ -123,7 +121,7 @@
 --     )
 -- )
 
--- 10.	Môn “Cau Truc Roi Rac” là môn bắt buộc phải học liền 
+-- III.10 Môn “Cau Truc Roi Rac” là môn bắt buộc phải học liền 
 -- trước những môn học (mã môn học, tên môn học) nào.
 
 -- SELECT MAMH,TENMH FROM MONHOC AS MH
@@ -134,3 +132,19 @@
 --         WHERE TENMH='Cau Truc Roi Rac'
 --     )
 -- )
+
+-- 4. Bài tập 4
+-- III.14
+
+-- SELECT MASP, TENSP
+-- FROM SANPHAM 
+-- WHERE NUOCSX = 'Trung Quoc' OR MASP IN (
+-- 	SELECT DISTINCT CT.MASP 
+-- 	FROM CTHD CT INNER JOIN HOADON HD
+-- 	ON CT.SOHD = HD.SOHD
+-- 	WHERE NGHD = '01/01/2007'
+-- )
+
+-- III.15
+-- SELECT MASP,TENSP FROM SANPHAM
+-- WHERE MASP NOT IN (SELECT DISTINCT MASP FROM CTHD )
